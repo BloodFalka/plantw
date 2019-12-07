24d3b372f92bb986f9c9a58bba33f8b27dc42444
BloodFalka <57503894+BloodFalka@users.noreply.github.com>
2019-12-07 17:13:51 +0200

    funct
    function

--- js/script.js
+++ js/script.js
@@ -1,49 +1,10 @@
-var bullingObjects = [
-	"Бізнес",
-	"Ніс",
-	"Батько",
-	"Палець",
-	"Браслет"
-];
+function chooseRandom(words){
+	return words[Math.floor(Math.random()* words.length)]};
 
-var bullings = [
-	"Смердючий",
-	"Гівняний",
-	"Прищавий",
-	"Гнилий",
-	"Хуйоватий",
-	"Їбаний"
-];
+var bullingObjects = [ "Бізнес", "Ніс", "Батько", "Палець", "Браслет"];
+var bullings = ["Смердючий", "Гівняний", "Прищавий", "Гнилий", "Хуйоватий", "Їбаний"];
+var bullingWords = ["Член", "Кіт", "Хомяк", "Тінейджер", "Морозильник"];
 
-var bullingWords = [
-	"Член",
-	"Кіт",
-	"Хомяк",
-	"Тінейджер",
-	"Морозильник"
-];
-
-var bullingObject = bullingObjects[Math.floor(Math.random()* bullingObjects.length)];
-var bulling = bullings[Math.floor(Math.random() * bullings.length)];
-var bullingWord = bullingWords[Math.floor(Math.random() * bullingWords.length)];
-
-var badWord = ["Твій", bullingObject, bulling, bullingWord + "!!!"].join(" ");
+var badWord = ["Твій", chooseRandom(bullingObjects), chooseRandom(bullings), chooseRandom(bullingWords) + "!!!"].join(" ");
 
 alert(badWord);
-
-var name = [
-"Антоні Улай",
-"Людаміла",
-"Назаріі",
-"Оліяша",
-"Галінда"
-];
-console.log(name);
-
-var randomName = name[Math.floor(Math.random() * name.length)];
-
-console.log("Привіт " + randomName);
-
-if (randomName.length >= 7){
-  console.log("Вав, твій дік большой");
-};
